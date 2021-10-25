535d5ec693b900c7f47ed1a3c238c23b1e2ce83c
Hasim Sait Goktan <hasimsait@sabanciuniv.edu>
2021-10-26 02:28:22 +0300

    added credits. Some sleep to get it below 60fps

--- screenshot/screenshot_fast.cpp
+++ screenshot/screenshot_fast.cpp
@@ -8,7 +8,11 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <time.h>
+#include <unistd.h>
 
+// Fast C++ screenshots by étale-cohomology
+// https://stackoverflow.com/questions/24988164/c-fast-screenshots-in-linux-for-use-with-opencv/39781697
 // Using one monitor DOESN'T improve performance! Querying a smaller subset of the screen DOES
 const uint WIDTH  = 1920>>0;
 const uint HEIGHT = 1080>>0;
@@ -30,16 +34,22 @@ int main(){
         puts("Fatal shminfo error!");;
     Status s1 = XShmAttach(display, &shminfo);
     printf("XShmAttach() %s\n", s1 ? "success!" : "failure!");
-
-    //cv::Mat img;
-
-    for(int i; ; i++){
-            XShmGetImage(display, root, ximg, 0, 0, 0x00ffffff);
-            if (ximg ->data != NULL)
-              saveXImageToBitmap(ximg);
-            //img = cv::Mat(HEIGHT, WIDTH, CV_8UC4, ximg->data);
-            break;
-        }
+    double start = clock();
+    double end;
+    for(int i=0;i<10 ; i++){
+      XShmGetImage(display, root, ximg, 0, 0, 0x00ffffff);
+      if (ximg ->data != NULL)
+        saveXImageToBitmap(ximg);
+      // this is bad, you should use cond timed wait to get fixed framerate.
+      // screenshot instead of drm is a bad idea.
+      // 0.018 is the fastest I had, 56fps is fine for me.
+      // making the sample program read from ram
+      // has higher prio for me than fixed framerate.
+      sleep(0.01);
+      end = clock();
+      std::cout<<"time "<< (end-start)/CLOCKS_PER_SEC<<std::endl;
+      start = clock();
+    }
 
     XShmDetach(display, &shminfo);
     XDestroyImage(ximg);
@@ -47,6 +57,10 @@ int main(){
     XCloseDisplay(display);
     puts("Exit success!");
 }
+
+// Saving an XImage as a bitmap by Tony Zhu
+// https://stackoverflow.com/questions/34943928/can-not-save-screen-shot-bitmap-to-file-using-xlib
+
 #pragma pack (1)
 typedef struct BITMAPFILEHEADER 
 {
@@ -87,17 +101,13 @@ bmpFileHeader.bfReserved1 = 0;
 bmpFileHeader.bfReserved2 = 0;
 int biBitCount =32;
 int dwBmpSize = ((pImage->width * biBitCount + 31) / 32) * 4 * pImage->height;
-printf("size of short:%d\r\n",(int)sizeof(short));
-printf("size of int:%d\r\n",(int)sizeof(int));
-printf("size of long:%d\r\n",(int)sizeof(long));
-printf("dwBmpSize:%d\r\n",(int)dwBmpSize);
-printf("BITMAPFILEHEADER:%d\r\n",(int)sizeof(BITMAPFILEHEADER));
-printf("BITMAPINFOHEADER:%d\r\n",(int)sizeof(BITMAPINFOHEADER));
 bmpFileHeader.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) +  dwBmpSize;
 
 bmpInfoHeader.biSize = sizeof(BITMAPINFOHEADER);
 bmpInfoHeader.biWidth = pImage->width;
 bmpInfoHeader.biHeight = -1*pImage->height;
+// sample bitmaps are displayed correctly while
+// bitmaps we save are upside down, -height fixes that.
 bmpInfoHeader.biPlanes = 1;
 bmpInfoHeader.biBitCount = biBitCount;
 bmpInfoHeader.biSizeImage = 0;
